32e614f2617598fe9e99dfa20b8026dea22f62ea
Labib <labib.rulz@gmail.com>
2020-03-21 21:19:41 +0600

    js file changed image now works dynamically

--- button.js
+++ button.js
@@ -1,10 +1,16 @@
+const image1 = document.createElement("img");
+image1.src = "images/1.jpg";
+const image2 = document.createElement("img");
+image2.src = "images/2.jpg";
+const image3 = document.createElement("img");
+image3.src = "images/3.jpg";
+const image4 = document.createElement("img");
+image4.src = "images/4.jpg";
+const image5 = document.createElement("img");
+image5.src = "images/52698949_1014030908795010_1409213932714852352_n.JPG.jpg";
 function myFunction() {
     var pics = [
-        "file:///C:/Users/Labz/Downloads/Compressed/project%201-20200321T062704Z-001/project%201/images/52698949_1014030908795010_1409213932714852352_n.JPG",
-        "file:///C:/Users/Labz/Downloads/Compressed/project%201-20200321T062704Z-001/project%201/images/1.jpg",
-        "file:///C:/Users/Labz/Downloads/Compressed/project%201-20200321T062704Z-001/project%201/images/2.jpg",
-        "file:///C:/Users/Labz/Downloads/Compressed/project%201-20200321T062704Z-001/project%201/images/3.jpg",
-        "file:///C:/Users/Labz/Downloads/Compressed/project%201-20200321T062704Z-001/project%201/images/4.jpg"
+       image1.src, image2.src,image3.src,image4.src,image5.src
     ]
     var image = document.getElementsByClassName("image")[0];
     var arr_index=(Math.floor(Math.random()*5));
